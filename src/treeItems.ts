b42e34697a004f39eb5dc266636efc686ef119b9
kpkym <okpkym@gmail.com>
2026-03-02 15:05:16 +0800

    feat: add tree item classes

---
+++ src/treeItems.ts
@@ -0,0 +1,34 @@
+import * as vscode from 'vscode';
+import { Group, Project } from './types';
+
+export class GroupItem extends vscode.TreeItem {
+  readonly contextValue = 'group'; // MUST match viewItem == group in package.json
+
+  constructor(public readonly group: Group) {
+    super(group.name, vscode.TreeItemCollapsibleState.Expanded);
+    this.tooltip = group.name;
+    this.iconPath = new vscode.ThemeIcon('folder');
+  }
+}
+
+export class ProjectItem extends vscode.TreeItem {
+  readonly contextValue = 'project'; // MUST match viewItem == project in package.json
+
+  constructor(
+    public readonly project: Project,
+    public readonly groupName: string
+  ) {
+    super(project.name, vscode.TreeItemCollapsibleState.None);
+    this.tooltip = project.path;
+    this.description = project.path;
+    this.iconPath = new vscode.ThemeIcon('repo');
+    // Left-click triggers openProject command
+    this.command = {
+      command: 'projectManager.openProject',
+      title: 'Open Project',
+      arguments: [this],
+    };
+  }
+}
+
+export type TreeNode = GroupItem | ProjectItem;
